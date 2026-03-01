45fe48e29948c5670bff121d72098fdf469e51dc
Bloedrenn <bloedrenn@gmail.com>
2026-03-01 11:09:00 +0300

    feat(blog/news): Кастомизировал админку для модели News

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,13 @@ admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Comment)
-admin.site.register(News)
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['post_title', 'is_important', 'news_type', 'pinned', 'email_notifications_sent'] # Можно так: post_item__title
+    list_filter = ['is_important', 'news_type', 'pinned', 'email_notifications_sent']
+    
+    def post_title(self, obj):
+        return obj.post_item.title
+    post_title.short_description = 'Заголовок'
