cea3e2d61c0b3d3013414d638630f1feb8d8341d
Bloedrenn <bloedrenn@gmail.com>
2026-03-14 00:24:05 +0300

    Revert "feat(admin): Кастомизировать админку, jazzmin"

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,14 +48,6 @@ INSTALLED_APPS = [
     'blog',
     'users'
 ]
-
-JAZZMIN_SETTINGS = {
-    "site_title": "Админка", # Заголовок окна браузера
-    "site_brand": "Pikabu", # Бренд в меню
-    "welcome_sign": "Добро пожаловать",
-    "copyright": "Pikabu",
-    "search_model": ["users.CustomUser"], # Глобальный поиск по моделям
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
